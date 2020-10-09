d07551940d8feae4ea32f638c879f2557ff5549a
Tushar <tushar110700@gmail.com>
2020-10-09 20:22:48 +0530

    Entering Files

---
+++ app.py
@@ -0,0 +1,47 @@
+
+from flask import Flask, render_template, request, jsonify
+import requests
+from datetime import datetime
+import pymongo
+
+app=Flask(__name__)
+@app.route('/', methods=['GET','POST'])
+def signup():
+    return render_template('signup.html')
+@app.route('/homepage_sign_up',methods=['POST'])
+def homepage_signup():
+
+
+
+    return render_template("index.html")
+@app.route('/homepage_signin',methods=['POST'])
+def homepage_signin():
+
+
+    return render_template("index.html")
+
+
+@app.route('/result',methods=['POST'])
+def result():
+    url = request.form['url']
+    fromm = request.form['from']
+    host = request.form['host']
+    headers={'From':fromm , 'Host': host}
+    dt=datetime.now()
+    req=requests.get("https://www.youtube.com/",headers=headers)
+    headers['dt']=dt
+
+    res_headers = req.headers
+    res_headers['dt']=dt
+    DB_NAME = 'CN_LAB'
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    database = client[DB_NAME]
+    collection1 = database['Requests']
+    collection = database['Responses']
+    collection.insert_one(res_headers)
+    collection1.insert_one(headers)
+    res_headers.pop('dt')
+    return render_template('result.html', res_headers=res_headers)
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)
