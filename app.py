3087b93959d45a3690edeadd6678efe82841b92d
Shadan Zahid <Shadan.zahid123@gmail.com>
2020-10-12 16:12:54 +0530

    done exception handling and worked on making website more realistics

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import pymongo
 
 app=Flask(__name__)
-@app.route('/', methods=['GET','POST'])
+@app.route('/register', methods=['GET','POST'])
 def register():
     return render_template('register.html')
 
@@ -13,40 +13,46 @@ def register():
 def login():
     return render_template('login.html')
 
-@app.route('/homepage_sign_up',methods=['POST','GET'])
+@app.route('/',methods=['POST','GET'])
 def homepage_signup():
 
     return render_template("index.html")
 
-
-@app.route('/homepage_signin',methods=['POST'])
-def homepage_signin():
-
-
-    return render_template("index.html")
-
-
 @app.route('/result',methods=['POST'])
 def result():
-    url = request.form['url']
-    fromm = request.form['from']
-    host = request.form['host']
-    headers={'From':fromm , 'Host': host}
-    dt=datetime.now()
-    req=requests.get("https://www.youtube.com/",headers=headers)
-    headers['dt']=dt
+    try:
+        url = request.form['url']
+        if(url[0:4] != 'http' or url[0:5] != "https"):
+            url = "http://" + url
+        print(url)
+        fromm = ""
+        try:
+            fromm = request.form['from']
+        except Exception:
+            fromm = ""
+        host = ""
+        try:
+            host = request.form['host']
+        except Exception:
+            host = ""
+        headers={'From':fromm , 'Host': host}
+        dt=datetime.now()
+        req=requests.get(url)
+        #headers['dt']=dt
 
-    res_headers = req.headers
-    res_headers['dt']=dt
-    DB_NAME = 'CN_LAB'
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
-    database = client[DB_NAME]
-    collection1 = database['Requests']
-    collection = database['Responses']
-    collection.insert_one(res_headers)
-    collection1.insert_one(headers)
-    res_headers.pop('dt')
-    return render_template('result.html', res_headers=res_headers)
-
+        res_headers = req.headers
+        #res_headers['dt']=dt
+        #DB_NAME = 'CN_LAB'
+        #client = pymongo.MongoClient('mongodb://localhost:27017/')
+        #database = client[DB_NAME]
+        #collection1 = database['Requests']
+        #collection = database['Responses']
+        #collection.insert_one(res_headers)
+        #collection1.insert_one(headers)
+        #res_headers.pop('dt')
+        content = {"res_headers":res_headers,"r_url":req.url,'req':req}
+        return render_template('result.html',**content)
+    except Exception:
+        return render_template('index.html',query="No such url exist!")
 if __name__ == "__main__":
     app.run(port=8080,debug=True)
